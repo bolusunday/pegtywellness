5b163639e1e939a3098a618d179a78050ea21598
bolusunday <adebolusunday86@gmail.com>
2026-09-16 19:20:41 +0100

    Giscus theme updated

--- src/components/Comments.jsx
+++ src/components/Comments.jsx
@@ -20,7 +20,7 @@ export default function Comments({ slug }) {
         reactionsEnabled="1"
         emitMetadata="0"
         inputPosition="top"
-        theme="light"
+        theme="noborder_gray"
         lang="en"
         loading="lazy"
       />
